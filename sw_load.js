cfb25fee557efc7fb993b3763d16e6b93f6c73fb
Jean Hugues Robert <jean_hugues_robert@yahoo.com>
2026-07-10 20:33:10 +0200

    Fix landing page loader initialization

--- sw_load.js
+++ sw_load.js
@@ -1,12 +1,3 @@
-var _____WB$wombat$assign$function_____=function(name){return (globalThis._wb_wombat && globalThis._wb_wombat.local_init && globalThis._wb_wombat.local_init(name))||globalThis[name];};if(!globalThis.__WB_pmw){globalThis.__WB_pmw=function(obj){this.__WB_source=obj;return this;}}{
-let window = _____WB$wombat$assign$function_____("window");
-let self = _____WB$wombat$assign$function_____("self");
-let document = _____WB$wombat$assign$function_____("document");
-let location = _____WB$wombat$assign$function_____("location");
-let top = _____WB$wombat$assign$function_____("top");
-let parent = _____WB$wombat$assign$function_____("parent");
-let frames = _____WB$wombat$assign$function_____("frames");
-let opener = _____WB$wombat$assign$function_____("opener");
 function loadfire( src, not_async ){
 
  
@@ -523,25 +514,3 @@ function loadfire( src, not_async ){
  
   return me
 }
-}
-
-/*
-     FILE ARCHIVED ON 15:28:44 Dec 25, 2014 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 03:55:04 Jul 05, 2026.
-     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
-
-     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
-     SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  captures_list: 0.459
-  exclusion.robots: 0.047
-  exclusion.robots.policy: 0.037
-  esindex: 0.008
-  cdx.remote: 5.896
-  LoadShardBlock: 151.51 (3)
-  PetaboxLoader3.datanode: 69.46 (4)
-  PetaboxLoader3.resolve: 98.608 (2)
-  load_resource: 93.234
-*/
